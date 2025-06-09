8f9829165e7b4a254d9866c50c07de17ca4c7219
Zanna-dev <pamn2704@gmail.com>
2025-06-10 00:45:24 +0100

    Create the First Employee File

---
+++ src/app/component/add-employee/add-employee.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-add-employee',
+  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatDatepickerModule],
+  templateUrl: './add-employee.component.html',
+  styleUrl: './add-employee.component.css'
+})
+export class AddEmployeeComponent {
+  title = 'Add Employee'
+  empForm = new FormGroup({
+    id: new FormControl(0),
+    name: new FormControl('', Validators.required),
+    doj: new FormControl(new Date(), Validators.required),
+    role: new FormControl('', Validators.required),
+    salary: new FormControl(0, Validators.required)
+  })
+
+  SaveEmployee() {
+    if (this.empForm.valid) {
+
+    }
+  }
+}
