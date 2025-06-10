a4f81990075dce42bce21f3494d3d6984a3b0bf2
Zanna-dev <pamn2704@gmail.com>
2025-06-10 22:31:34 +0100

    Update the Employee Registration Form

--- src/app/component/add-employee/add-employee.component.ts
+++ src/app/component/add-employee/add-employee.component.ts
@@ -6,14 +6,25 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Employee } from '../../model/Employee';
+import { EmployeeService } from '../../service/employee.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatDatepickerModule],
+  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatDatepickerModule, MatIconModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
 export class AddEmployeeComponent {
+
+  constructor(private service:EmployeeService,private ref:MatDialogRef<AddEmployeeComponent>){
+
+  }
+
   title = 'Add Employee'
   empForm = new FormGroup({
     id: new FormControl(0),
@@ -25,7 +36,21 @@ export class AddEmployeeComponent {
 
   SaveEmployee() {
     if (this.empForm.valid) {
-
+        let _data:Employee={
+          id: this.empForm.value.id as number,
+          name: this.empForm.value.name as string,
+          doj: new Date(this.empForm.value.doj as Date),
+          role: this.empForm.value.role as string,
+          salary: this.empForm.value.salary as number,
+        }
+        this.service.Create(_data).subscribe(item=>{
+          alert('saved')
+          this.closepopup();
+        });
     }
+  }
+
+  closepopup(){
+    this.ref.close();
   }
 }
